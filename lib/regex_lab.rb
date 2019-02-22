06fcb32ae1723e6450375b951f1b5ef8362b069d
Eyal Toledano <eyaltoledano@me.com>
2019-02-22 15:47:32 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  if word[0] == word[0].match(/[aeoiu|AEIOU]/).to_s
+    true
+  elsif word[0] == word[0].match(/[bcdfghjklmnpqrstvwxyz|BCDFGHJKLMNPQRSTVWXYZ]/).to_s
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w[a-z]{4}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!text.match(/^[A-Z].*\W$/)
 end
 
 def valid_phone_number?(phone)
-
+  binding.pry
+  phone.match(/\A\d{10}\z/) ? true : false
 end
+
+#  phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
